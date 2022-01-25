94837599da62c8877453a0439f772292d82e35cf
willehpeh <will_alexander@me.com>
2022-01-25 19:00:04 +0100

    Comments are animated

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,21 +1,81 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Comment } from '../../../core/models/comment.model';
+import { animate, AnimationEvent, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
+  animations: [
+    trigger('listItem', [
+      state('default', style({
+        transform: 'scale(1)',
+        backgroundColor: 'white',
+        zIndex: 1
+      })),
+      state('raised', style({
+        transform: 'scale(1.05)',
+        backgroundColor: 'rgb(201, 157, 242)',
+        zIndex: 2
+      })),
+      state('delete', style({
+        transform: 'scale(1.05)',
+        backgroundColor: 'rgb(255, 77, 77)',
+        zIndex: 2
+      })),
+      state('deleted', style({
+        transform: 'translateX(100%)',
+        opacity: 0
+      })),
+      transition('default => raised', [
+        animate('100ms ease-in-out')
+      ]),
+      transition('raised => default', [
+        animate('500ms ease-in-out')
+      ]),
+      transition('* => delete', [
+        animate('2000ms ease-in-out')
+      ]),
+      transition('delete => *', [
+        animate('200ms')
+      ]),
+      transition(':enter', [
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0
+        }),
+        animate('250ms ease-out', style({
+          transform: 'translateX(0)',
+          opacity: 1
+        }))
+      ])
+    ])
+  ]
 })
 export class CommentsComponent implements OnInit {
 
   commentCtrl!: FormControl;
   @Input() comments!: Comment[];
   @Output() commentSubmitted = new EventEmitter<string>();
+  raisedIndex = -1;
+  animationStatuses: { [key: number]: 'raised' | 'default' | 'delete' | 'deleted' } = {};
+  deleteTimer!: number;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initFormControl();
+    this.setAnimationStatuses();
+  }
+
+  setAnimationStatuses() {
+    for (let i in this.comments) {
+      this.animationStatuses[i] = 'default';
+    }
+  }
+
+  initFormControl() {
     this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)]);
   }
 
@@ -38,6 +98,14 @@ export class CommentsComponent implements OnInit {
     this.commentCtrl.reset();
   }
 
+  onMouseEnterComment(index: number) {
+    this.animationStatuses[index] = 'raised';
+  }
+
+  onMouseLeaveComment(index: number) {
+    this.animationStatuses[index] = 'default';
+  }
+
   getFormError(): string {
     if (this.commentCtrl.hasError('required')) {
       return 'Vous devez écrire quelque chose !'
@@ -46,5 +114,31 @@ export class CommentsComponent implements OnInit {
     } else {
       return 'Erreur inconnue !';
     }
+  }
+
+  deleteComment(index: number) {
+    this.comments.splice(index, 1);
+  }
+
+  startDeleteComment(index: number) {
+    this.animationStatuses[index] = 'delete';
+    this.deleteTimer = setTimeout(() => {
+      this.animationStatuses[index] = 'deleted';
+    }, 2000);
+  }
+
+  stopDeleteComment(index: number) {
+    if (this.animationStatuses[index] === 'delete') {
+      this.animationStatuses[index] = 'raised';
+      clearTimeout(this.deleteTimer);
+    }
+  }
+
+  onDeletedAnimationEnd(animationEvent: AnimationEvent, index: number) {
+    if (animationEvent.toState !== 'deleted') {
+      return;
+    }
+    this.deleteComment(index);
+    this.setAnimationStatuses();
   }
 }
